7b025a1bb5444e6fcdc34444dc2992bbbac1c875
Chirag Sharma <128040209+chiragsharma27@users.noreply.github.com>
2024-12-29 20:12:07 +0530

    Delete src/pages directory

--- src/pages/_app.js
+++
@@ -1,6 +0,0 @@
-// import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-// import '../styles/globals.css'; // Your custom global styles
-
-// export default function App({ Component, pageProps }) {
-//   return <Component {...pageProps} />;
-// }
